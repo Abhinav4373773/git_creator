df3d954cd00c1fcadbe81c8127ed174ad70af895
Abhinav4373773 <120887314+Abhinav4373773@users.noreply.github.com>
2025-05-17 20:11:14 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,19 @@
+import axios from "axios";
+// import { useEffect } from "react";
+import Random  from './components/Random'
+import Tag from './components/Tag'
+
+export default function App() {
+ 
+  return (
+      <div className="w-full h-screen flex flex-col background relative overflow-x-hidden items-center">
+        <h1 className="bg-white rounded-lg w-11/12 text-center px-10 py-2 text-4xl font-bold mt-[40px]">
+          Random Gif
+        </h1>
+        <div className="flex flex-col w-full items-center gap-y-10 mt-[30px]">
+          <Random />
+          <Tag />
+        </div>
+      </div>
+    );
+}
